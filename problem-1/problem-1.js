3a614070b5856b2af1e3ecb15a17267be2cceb2e
Dominique Morris <morrisdominique16@yahoo.com>
2022-05-18 19:20:55 -0400

    Problem 1 first attempt

---
+++ problem-1/problem-1.js
@@ -0,0 +1,24 @@
+//Create a sample object, then write a JavaScript function that 
+//deletes the property of an object passed to it.
+
+/*
+Step 1: Define a sample object
+Step 2: Define a function that takes in two parameters, the object and property.
+Step 3: The function will delete the property out of the object based on what is 
+passed in using the "delete" keyword.
+*/
+
+const vehicle = {
+  brand: "Subaru",
+  model: "WRX",
+  year: 2017,
+  color: "Blue"
+};
+
+function propertyRemover(object, propery){
+  delete object.property;
+  
+  return "this car is " + object.property;
+}
+
+console.log(propertyRemover(vehicle, vehicle.year));
