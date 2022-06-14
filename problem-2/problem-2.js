6604064272c95aee86bcc0977d2feb7cd7a1c958
Dominique Morris <morrisdominique16@yahoo.com>
2022-06-14 19:57:34 -0400

    First iteration for problem 2

---
+++ problem-2/problem-2.js
@@ -0,0 +1,41 @@
+//Create an array of people objects with first name, last name, and age, 
+//then write a JavaScript function that takes in an array and console logs
+//the first and last names of all the people.
+
+
+/*
+step 1: create people objects that include first name, last name, and age. 
+step 2: create an array that holds the people objects within itself 
+step 3: create a function that takes in the array as a parameter and will loop through
+the array of objects. 
+step 4: console.log the first and last names within each object in the array.
+*/
+
+
+const person1 = {
+  firstName: "John", 
+  lastName: "Deer", 
+  age: 35
+};
+
+const person2 = {
+  firstName: "Jane", 
+  lastName: "Doe", 
+  age: 34
+};
+
+const person3 = {
+  firstName: "Fawn", 
+  lastName: "Dior", 
+  age: 6
+};
+
+const arrayOfPeople = [person1, person2, person3];
+
+function nameIdentifier(array) {
+  for (let i = 0; i < array.length; i++){
+     return "First Name: " + array.firstName + " Last Name: " + array.lastName; 
+  }
+}
+
+console.log(nameIdentifier(arrayOfPeople));
