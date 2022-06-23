c4d3a47be8f27a15d7f7d7a4fd89ce606e13eeb9
Dominique Morris <morrisdominique16@yahoo.com>
2022-06-22 20:53:28 -0400

    Problem 4 using an object instead of a map

--- problem-4/problem-4.js
+++ problem-4/problem-4.js
@@ -1,25 +1,24 @@
+
 //Create an object, then write a JavaScript function that checks whether an 
 //object contains the specified key.
 
 /*
-Step 1: Create an object map variable.
-Step 2: Create a function that takes in two parameters, the object map variable and
+Step 1: Create an object.
+Step 2: Create a function that takes in two parameters, the object and
 a specified key value. 
-Step 3: Use the Map.has() method in the function to check if the variable passed in 
-the first parameter contains the specified key that was passed in the second parameter.
+Step 3: Using the hasOwnProperty will check to see if the passed in string key value is
+within the object passed in. 
 */
 
-const vacationDestinations = new Map([
-  ["Moroco", 500],
-  ["Mykonos", 840],
-  ["Brazil", 675], 
-  ["Ecuador", 925]
-]);
 
-function vacationDestinationChecker(vacationPackage, location) {
-  let text = "Does this Package contain " + location + "? " + vacationPackage.has(location);
-  
-  return text;
+const myObject = {
+  firstName: "Dominique", 
+  lastName: "Morris", 
+  age: 26
+};
+
+function objectKeyChecker(obj, key) {
+  return obj.hasOwnProperty(key);
 }
 
-console.log(vacationDestinationChecker(vacationDestinations, "Brazil"));
+console.log(objectKeyChecker(myObject, "last"));
